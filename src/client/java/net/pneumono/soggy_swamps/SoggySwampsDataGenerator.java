cb0612fa603e0cdfab57f4fda62c361488229b46
Pneumono_ <pneumono@zohomail.com>
2025-08-08 06:04:01 +0100

    Re-add datagen

---
+++ src/client/java/net/pneumono/soggy_swamps/SoggySwampsDataGenerator.java
@@ -0,0 +1,13 @@
+package net.pneumono.soggy_swamps;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SoggySwampsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+
+	}
+}
