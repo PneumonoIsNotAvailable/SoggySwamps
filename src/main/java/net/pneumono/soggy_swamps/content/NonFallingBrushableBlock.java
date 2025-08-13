a7537cb003ac6871c6eb8d080ea0d331043f75f2
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-08-13 07:25:32 +0100

    Fix issues with NonFallingBrushableBlock

--- src/main/java/net/pneumono/soggy_swamps/content/NonFallingBrushableBlock.java
+++ src/main/java/net/pneumono/soggy_swamps/content/NonFallingBrushableBlock.java
@@ -1,75 +1,16 @@
 package net.pneumono.soggy_swamps.content;
 
-import com.mojang.serialization.MapCodec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.block.*;
-import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BrushableBlockEntity;
-import net.minecraft.registry.Registries;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.IntProperty;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldView;
-import net.minecraft.world.tick.ScheduledTickView;
-import org.jetbrains.annotations.Nullable;
 
-public class NonFallingBrushableBlock extends BlockWithEntity {
-    public static final MapCodec<NonFallingBrushableBlock> CODEC = RecordCodecBuilder.mapCodec(
-            instance -> instance.group(
-                            Registries.BLOCK.getCodec().fieldOf("turns_into").forGetter(NonFallingBrushableBlock::getBaseBlock),
-                            Registries.SOUND_EVENT.getCodec().fieldOf("brush_sound").forGetter(NonFallingBrushableBlock::getBrushingSound),
-                            Registries.SOUND_EVENT.getCodec().fieldOf("brush_completed_sound").forGetter(NonFallingBrushableBlock::getBrushingCompleteSound),
-                            createSettingsCodec()
-                    )
-                    .apply(instance, NonFallingBrushableBlock::new)
-    );
-    private static final IntProperty DUSTED = Properties.DUSTED;
-    private final Block baseBlock;
-    private final SoundEvent brushingSound;
-    private final SoundEvent brushingCompleteSound;
-
-    @Override
-    public MapCodec<NonFallingBrushableBlock> getCodec() {
-        return CODEC;
-    }
-
-    public NonFallingBrushableBlock(Block baseBlock, SoundEvent brushingSound, SoundEvent brushingCompleteSound, AbstractBlock.Settings settings) {
-        super(settings);
-        this.baseBlock = baseBlock;
-        this.brushingSound = brushingSound;
-        this.brushingCompleteSound = brushingCompleteSound;
-        this.setDefaultState(this.stateManager.getDefaultState().with(DUSTED, 0));
-    }
-
-    @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(DUSTED);
-    }
-
-    @Override
-    public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
-        world.scheduleBlockTick(pos, this, 2);
-    }
-
-    @Override
-    public BlockState getStateForNeighborUpdate(
-            BlockState state,
-            WorldView world,
-            ScheduledTickView tickView,
-            BlockPos pos,
-            Direction direction,
-            BlockPos neighborPos,
-            BlockState neighborState,
-            Random random
-    ) {
-        tickView.scheduleBlockTick(pos, this, 2);
-        return super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
+public class NonFallingBrushableBlock extends BrushableBlock {
+    public NonFallingBrushableBlock(Block baseBlock, SoundEvent brushingSound, SoundEvent brushingCompleteSound, Settings settings) {
+        super(baseBlock, brushingSound, brushingCompleteSound, settings);
     }
 
     @Override
@@ -79,21 +20,8 @@ public class NonFallingBrushableBlock extends BlockWithEntity {
         }
     }
 
-    @Nullable
     @Override
-    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new BrushableBlockEntity(pos, state);
-    }
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
 
-    public Block getBaseBlock() {
-        return this.baseBlock;
-    }
-
-    public SoundEvent getBrushingSound() {
-        return this.brushingSound;
-    }
-
-    public SoundEvent getBrushingCompleteSound() {
-        return this.brushingCompleteSound;
     }
 }
