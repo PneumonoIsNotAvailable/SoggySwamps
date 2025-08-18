e8cc391ca43f1e5a66e43ecc6e208339dba6cc0b
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-08-18 01:18:01 +0100

    Change Swamp Spider loot table

--- src/client/java/net/pneumono/soggy_swamps/datagen/SoggySwampsEntityLootTableProvider.java
+++ src/client/java/net/pneumono/soggy_swamps/datagen/SoggySwampsEntityLootTableProvider.java
@@ -6,6 +6,7 @@ import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.KilledByPlayerLootCondition;
+import net.minecraft.loot.entry.EmptyEntry;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.EnchantedCountIncreaseLootFunction;
 import net.minecraft.loot.function.SetCountLootFunction;
@@ -13,6 +14,7 @@ import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.registry.RegistryWrapper;
 import net.pneumono.soggy_swamps.registry.SoggySwampsEntities;
+import net.pneumono.soggy_swamps.registry.SoggySwampsItems;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
@@ -36,18 +38,15 @@ public class SoggySwampsEntityLootTableProvider extends FabricEntityLootTablePro
                         )
                         .pool(LootPool.builder()
                                 .rolls(ConstantLootNumberProvider.create(1.0F))
+                                .with(ItemEntry.builder(SoggySwampsItems.SWAMP_SPIDER_EYE)
+                                        .weight(2)
+                                )
                                 .with(ItemEntry.builder(Items.SPIDER_EYE)
-                                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(-1.0F, 1.0F)))
-                                        .apply(EnchantedCountIncreaseLootFunction.builder(this.registries, UniformLootNumberProvider.create(0.0F, 1.0F)))
                                 )
-                                .conditionally(KilledByPlayerLootCondition.builder())
-                        )
-                        .pool(LootPool.builder()
-                                .rolls(ConstantLootNumberProvider.create(1.0F))
-                                .with(ItemEntry.builder(Items.SLIME_BALL)
-                                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(-1.0F, 1.0F)))
-                                        .apply(EnchantedCountIncreaseLootFunction.builder(this.registries, UniformLootNumberProvider.create(0.0F, 1.0F)))
+                                .with(EmptyEntry.builder()
+                                        .weight(3)
                                 )
+                                .apply(EnchantedCountIncreaseLootFunction.builder(this.registries, UniformLootNumberProvider.create(0.0F, 1.0F)))
                                 .conditionally(KilledByPlayerLootCondition.builder())
                         )
         );
