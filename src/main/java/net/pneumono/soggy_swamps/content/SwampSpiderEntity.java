beb2ac31df13bdf5ef4d1101adeae9a6fe8b6aab
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-09-01 05:13:08 +0100

    Fix Swamp Spiders having a chance to spawn being ridden by regular Skeletons

--- src/main/java/net/pneumono/soggy_swamps/content/SwampSpiderEntity.java
+++ src/main/java/net/pneumono/soggy_swamps/content/SwampSpiderEntity.java
@@ -3,13 +3,17 @@ package net.pneumono.soggy_swamps.content;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.goal.FleeEntityGoal;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.BoggedEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.SpiderEntity;
 import net.minecraft.entity.passive.FrogEntity;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.Difficulty;
@@ -18,6 +22,8 @@ import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
 import net.pneumono.soggy_swamps.registry.SoggySwampsRegistry;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
 
 public class SwampSpiderEntity extends SpiderEntity {
     public SwampSpiderEntity(EntityType<? extends SpiderEntity> entityType, World world) {
@@ -61,15 +67,37 @@ public class SwampSpiderEntity extends SpiderEntity {
     @Nullable
     @Override
     public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData) {
-        entityData = super.initialize(world, difficulty, spawnReason, entityData);
-
         Random random = world.getRandom();
+
+        EntityAttributeInstance entityAttributeInstance = Objects.requireNonNull(this.getAttributeInstance(EntityAttributes.FOLLOW_RANGE));
+        if (!entityAttributeInstance.hasModifier(RANDOM_SPAWN_BONUS_MODIFIER_ID)) {
+            entityAttributeInstance.addPersistentModifier(
+                    new EntityAttributeModifier(
+                            RANDOM_SPAWN_BONUS_MODIFIER_ID, random.nextTriangular(0.0, 0.11485000000000001), EntityAttributeModifier.Operation.ADD_MULTIPLIED_BASE
+                    )
+            );
+        }
+
         if (random.nextInt(100) == 0) {
             BoggedEntity boggedEntity = EntityType.BOGGED.create(this.getWorld(), SpawnReason.JOCKEY);
             if (boggedEntity != null) {
                 boggedEntity.refreshPositionAndAngles(this.getX(), this.getY(), this.getZ(), this.getYaw(), 0.0F);
                 boggedEntity.initialize(world, difficulty, spawnReason, null);
                 boggedEntity.startRiding(this);
+            }
+        }
+
+        if (entityData == null) {
+            entityData = new SpiderEntity.SpiderData();
+            if (world.getDifficulty() == Difficulty.HARD && random.nextFloat() < 0.1F * difficulty.getClampedLocalDifficulty()) {
+                ((SpiderEntity.SpiderData)entityData).setEffect(random);
+            }
+        }
+
+        if (entityData instanceof SpiderEntity.SpiderData spiderData) {
+            RegistryEntry<StatusEffect> registryEntry = spiderData.effect;
+            if (registryEntry != null) {
+                this.addStatusEffect(new StatusEffectInstance(registryEntry, -1));
             }
         }
 
